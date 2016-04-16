8fd3d11d0ba5a3657ac590cd46cb856e3bfa4c69
Andrew Cho <choxandrew@gmail.com>
2016-04-16 17:18:28 -0500

    finished with google Trends

--- googleTrends.py
+++ googleTrends.py
@@ -12,7 +12,6 @@ with open("listFinal.txt", "r") as f:
     cards = [line.rstrip('\n') for line in f]
     
     for card in cards:
-        connector.request_report(card,geo="US")
+        connector.request_report(card,geo="US", date="today 90-d")
         connector.save_csv("./output/", card)
-    
         time.sleep(randint(4,8))
