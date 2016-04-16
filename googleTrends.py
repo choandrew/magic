ccdd9a777c66e19e5474569e588d0acbc55387a5
Andrew Cho <choxandrew@gmail.com>
2016-04-16 17:36:04 -0500

    put googleTrends.py in function

--- googleTrends.py
+++ googleTrends.py
@@ -2,16 +2,19 @@ from pytrends.pyGTrends import pyGTrends
 import time
 from random import randint
 
-googleUsername = "basedgodkiller@gmail.com"
-googlePassword = "Unicorn7!"
-
-#connect to Google
-connector = pyGTrends(googleUsername, googlePassword)
-
-with open("listFinal.txt", "r") as f:
-    cards = [line.rstrip('\n') for line in f]
+def main():
+    googleUsername = "basedgodkiller@gmail.com"
+    googlePassword = "Unicorn7!"
     
-    for card in cards:
-        connector.request_report(card,geo="US", date="today 90-d")
-        connector.save_csv("./output/", card)
-        time.sleep(randint(4,8))
+    #connect to Google
+    connector = pyGTrends(googleUsername, googlePassword)
+    
+    with open("listFinal.txt", "r") as f:
+        cards = [line.rstrip('\n') for line in f]
+        
+        for card in cards:
+            connector.request_report(card,geo="US", date="today 90-d")
+            connector.save_csv("./output/", card)
+            time.sleep(randint(4,8))
+
+
