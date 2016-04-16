b7da4af53703470ffbf9850034c092358c43519a
Andrew Cho <choxandrew@gmail.com>
2016-04-16 15:59:57 -0500

    added output folder

--- googleTrends.py
+++ googleTrends.py
@@ -1,6 +1,10 @@
 import pytrends
 
 
-
-
+with open("listFinal.txt", "r") as f:
+    cards = [line.rstrip('\n') for line in open('filename')]
+    
+    for card in cards:
+        csv = pytrends.request_report(keywords, hl=card, cat=None, geo="US", date=None, gprop=None)
+        pytrends.save_csv("./output", card)
 
