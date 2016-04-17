d09dbcc7e9e27d39f86d2255a45768e9c970fe4e
Andrew Cho <choxandrew@gmail.com>
2016-04-17 01:59:10 -0500

    done?

---
+++ main.py
@@ -0,0 +1,13 @@
+from listOfCardsSets.py import main()
+from listFutureOOR.py import main()
+from listOneWord.py import main()
+from listBanned.py import main()
+
+
+def main():
+    listOfCardsSets.py.main()
+    listFutureOOR.py.main()
+    listOneWord.py.main()
+    listBanned.py.main()
+
+
